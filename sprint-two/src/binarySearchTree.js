d148170b2ee7f40e5eb472fe04139063cdcdb33d
ghara537 <ghara537@gmail.com>
2017-10-13 20:07:21 -0700

    All sprint 2 implementations minus Hash Map

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,71 @@
 var BinarySearchTree = function(value) {
+  
+  var treeRoot = new treeNode(value);
+  console.log('treeRoot in bst:', JSON.stringify(treeRoot));
+  return treeRoot;
+
+};
+
+
+var treeNode = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+  this.leftCount = 0;
+  this.rightCount = 0;
+};
+
+treeNode.prototype.insert = function (value, parent) {
+  
+  console.log('first tree root:', this);
+  var parent = parent ? parent : this;
+  console.log('insert check:', parent.value);
+  if (!parent.value) {
+  
+    parent.value = value;
+    console.log('value of parent:', parent.value);
+  } else {
+    var parent = parent ? parent : this.treeRoot;
+    if ( value < parent.value) {
+      if (parent.left) {
+        this.insert(value, parent.left);
+      } else { 
+        parent.left = new treeNode(value);
+      }
+      parent.leftCount = parent.left.leftCount++;
+    } else {
+      if (parent.right) {
+        this.insert(value, parent.right);
+      } else {
+        parent.right = new treeNode(value);  
+      }
+      parent.rightCount = parent.right.rightCount++;
+    }
+  }
+};
+
+treeNode.prototype.contains = function (value, parent) {
+  var parent = parent ? parent : this;
+  if ( parent.value === value) {
+    return true;
+  } else if (value < parent.value && parent.left) { 
+    return this.contains(value, parent.left);
+  } else if (value > parent.value && parent.right) {
+    return this.contains(value, parent.right);
+  } else {
+    return false;
+  }
+};
+
+treeNode.prototype.depthFirstLog = function (cb, parent) {
+  var parent = parent ? parent : this;
+  parent.value = cb(parent.value);
+  if (parent.left) { 
+    this.depthFirstLog(cb, parent.left);
+  }
+  if (parent.right) {
+    this.depthFirstLog(cb, parent.right);
+  }
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,11 +6,26 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  var tuple = [k, v];
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if ( this.get(index) && this.retrieve(k)) {
+    var bucket = this.get(index);
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v; 
+      }
+    }
+  } else if (this.get(index) && !this.retrieve(k)) {
+    bucket.push(tuple);
+  } else {
+    var tupleArr = [];
+    bucket[index] = tupleArr.push(tuple);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
 };
 
 HashTable.prototype.remove = function(k) {
